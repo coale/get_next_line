774625c2a35c6d007611962798a3146ec562c809
coale <10050779+coale@users.noreply.github.com>
2022-12-14 10:17:42 +0100

    update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,101 +6,103 @@
 /*   By: aconta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 10:28:56 by aconta            #+#    #+#             */
-/*   Updated: 2022/12/10 09:40:27 by aconta           ###   ########.fr       */
+/*   Updated: 2022/12/14 08:35:00 by aconta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t  ft_strlen(const char *str)
 {
-	size_t	i;
-	size_t	c;
+        size_t  i;
 
-	i = 0;
-	c = 0;
-	while (dst[i] != '\0' && i < size)
-		i += 1;
-	while (src[c] != '\0' && (i + c) + 1 < size)
-	{
-		dst[i + c] = src[c];
-		c++;
-	}
-	if (i < size)
-		dst[i + c] = 0;
-	return (ft_strlen(src) + i);
+        i = 0;
+        while (str[i] != '\0')
+        {
+                i++;
+        }
+        return (i);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+size_t  ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	char	*newstr;
-	char	*p1;
-	char	*p2;
-	int		i;
+        size_t  i;
+        size_t  l;
 
-	p1 = (char *)s1;
-	p2 = (char *)s2;
-	newstr = malloc(sizeof (char) * (ft_strlen(p1) + ft_strlen(p2) + 1));
-	if (!newstr)
-		return (0);
-	i = 0;
-	while (p1[i] != '\0')
-	{
-		newstr[i] = p1[i];
-		i++;
-	}
-	newstr[i] = '\0';
-	ft_strlcat(newstr, p2, (ft_strlen(p1) + ft_strlen(p2) + 1));
-	return (newstr);
+        i = 0;
+        l = ft_strlen(src);
+        if (size != 0)
+        {
+                while (i < (size - 1) && src[i] != '\0')
+                {
+                        dst[i] = src[i];
+                        i++;
+                }
+                dst[i] = '\0';
+        }
+        return (l);
 }
 
-size_t	ft_strlen(const char *str)
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
+        size_t  i;
+        size_t  c;
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
+        i = 0;
+        c = 0;
+        while (dst[i] != '\0' && i < size)
+        {
+                i += 1;
+        }
+        while (src[c] != '\0' && (i + c) + 1 < size)
+        {
+                dst[i + c] = src[c];
+                c++;
+        }
+        if (i < size)
+                dst[i + c] = 0;
+        return (ft_strlen(src) + i);
 }
 
-char	*ft_substr(const char *s, unsigned int start, size_t len)
+char    *ft_strjoin(char *str, char *buff)
 {
-	size_t	i;
-	size_t	j;
-	char	*substr;
+        size_t  i;
+        size_t  j;
+        char    *new_str;
 
-	i = 0;
-	if (start >= ft_strlen(s))
-		j = 0;
-	else if (ft_strlen(s) - start < len)
-		j = ft_strlen(s) - start;
-	else
-		j = len;
-	substr = (char *) malloc(sizeof(char) * (j + 1));
-	if (!substr)
-		return (NULL);
-	while (i < j)
-	{
-		substr[i] = s[start];
-		i++;
-		start++;
-	}
-	substr[i] = '\0';
-	return (substr);
+        i = 1;
+        if (!str)
+        {
+                str = (char *)malloc(1 * sizeof(*str));
+                str[0] = '\0';
+        }
+        j = ft_strlen(str) + ft_strlen(buff);
+        if (!str || !buff)
+                return (NULL);
+        new_str = malloc((j + 1) * sizeof(*new_str));
+        if (new_str == NULL)
+                return (NULL);
+        if (str)
+                while (*str != '\0' && i++)
+                        *(new_str++) = *(str++);
+        while (*buff != '\0')
+                *(new_str++) = *(buff++);
+        *new_str = '\0';
+        str -= (i - 1);
+        free(str);
+        return (new_str - j);
 }
 
-int	find_index(char *s, char c)
+char    *ft_strchr(const char *s, int c)
 {
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-			return (i);
-		i++;
-	}
-	if (s[i] == c)
-		return (i);
-	return (-1);
+        if (!s)
+                return (0);
+        while (*s != (char)c)
+        {
+                if (*s == '\0')
+                {
+                        return (NULL);
+                }
+                s++;
+        }
+        return ((char *)s);
 }
